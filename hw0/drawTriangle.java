cd2720967db10c44de97a21d5c4a8dd6dd260e12
rachaelma9999 <rachelma4869@gmail.com>
2021-10-13 14:15:27 -0400

    new

---
+++ hw0/drawTriangle.java
@@ -0,0 +1,20 @@
+public class drawTriangle{
+    public static void main(String[] args) {
+        int col = 0;
+        int row = 0;
+        int SIZE = 5;
+        while (row <SIZE) {
+            while (col <= row) {
+                System.out.print('*');
+                col = col + 1;
+            }
+            while (col > row) {
+                System.out.println();
+                col = 0;
+                row = row + 1;
+            }
+        }
+        System.out.println();
+    }
+
+}
--- lab2/DebugPractice/DebugExercise3.java
+++ lab2/DebugPractice/DebugExercise3.java
@@ -2,7 +2,7 @@
  * Created by jug on 1/22/18.
  */
 public class DebugExercise3 {
-    public static int countTurnips(In in) {
+    public static int countTurnips(in in) {
         int totalTurnips = 0;
         while (!in.isEmpty()) {
             String vendor = in.readString();
